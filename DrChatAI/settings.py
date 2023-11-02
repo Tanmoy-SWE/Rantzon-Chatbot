6181cf54e820f4bc983202d4c6a3ab4ab54e3b75
Lamia45711704 <ahsanhabib3@iut-dhaka.edu>
2023-11-03 01:44:56 +0600

    Deploy

--- DrChatAI/settings.py
+++ DrChatAI/settings.py
@@ -112,9 +112,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/public/static/"
+STATIC_URL = "/public/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'public/static')
+    os.path.join(BASE_DIR, 'public/')
 ]
 MEDIA_ROOT = 'media/'
 # Default primary key field type
