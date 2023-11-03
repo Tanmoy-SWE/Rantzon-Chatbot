34a228df4e01143d2fd03679e6bd49ab3391f5ab
Lamia45711704 <ahsanhabib3@iut-dhaka.edu>
2023-11-03 12:06:42 +0600

    Chatbot Up & Running with CDN-1

--- chatBot/views.py
+++ chatBot/views.py
@@ -29,26 +29,26 @@ def read_text_from_file(filename):
 # View to handle user input and chatbot response
 def chatbot_view(request):
     if request.method == 'POST':
-        user_input = request.POST.get('user_input', '').strip()
+        # user_input = request.POST.get('user_input', '').strip()
 
-        if user_input.lower() in ['exit', 'quit', 'bye']:
-            return JsonResponse({'response': "Chatbot: Goodbye!"})
+        # if user_input.lower() in ['exit', 'quit', 'bye']:
+        #     return JsonResponse({'response': "Chatbot: Goodbye!"})
         
 
-        # Step 1: Read the text content from the file
-        filename = "media/rantzon.txt"  # Replace with the actual filename
-        preprocessed_text = read_text_from_file(filename)
+        # # Step 1: Read the text content from the file
+        # filename = "media/rantzon.txt"  # Replace with the actual filename
+        # preprocessed_text = read_text_from_file(filename)
 
-        # Initial conversation message with the system's introduction and the context from the file
-        conversation = [
-            {"role": "system", "content": f"You are a helpful assistant for Health related suggestion who predicts diseases. You can ask more information if needed."},
-            {"role": "user", "content": preprocessed_text},
-        ]
+        # # Initial conversation message with the system's introduction and the context from the file
+        # conversation = [
+        #     {"role": "system", "content": f"You are a helpful assistant for Health related suggestion who predicts diseases. You can ask more information if needed."},
+        #     {"role": "user", "content": preprocessed_text},
+        # ]
 
-        # Get the chatbot's response using the OpenAI API
-        chatbot_output = get_chatbot_response(user_input, conversation)
+        # # Get the chatbot's response using the OpenAI API
+        # chatbot_output = get_chatbot_response(user_input, conversation)
 
-        return JsonResponse({'response': "Chatbot: " + chatbot_output})
+        return JsonResponse({'response': "Chatbot: " + "chatbot_output"})
 
     return render(request, 'chatbot.html')
 
