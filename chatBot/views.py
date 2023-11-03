1aace627a6830bee666841acf60bc12027e707f9
Lamia45711704 <ahsanhabib3@iut-dhaka.edu>
2023-11-03 12:04:38 +0600

    Chatbot Up & Running with CDN

--- chatBot/views.py
+++ chatBot/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import openai
-import time
 
 
 openai.api_key = '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'
@@ -11,17 +10,17 @@ openai.api_key = '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'
 def get_chatbot_response(user_message, conversation):
     # Append user message to the conversation
     conversation.append({"role": "user", "content": user_message})
+    return "Resp"
+    # # Get the chatbot's response from the API
+    # response = openai.ChatCompletion.create(
+    #     engine="text-davinci-002", 
+    #     messages=conversation
+    # )
 
-    # Get the chatbot's response from the API
-    response = openai.ChatCompletion.create(
-        engine="text-davinci-002", 
-        messages=conversation
-    )
-
-    # Extract and return the chatbot's response
-    chatbot_response = response['choices'][0]['message']['content']
-    conversation[-1]['content'] = chatbot_response  # Update conversation with the chatbot response
-    return chatbot_response
+    # # Extract and return the chatbot's response
+    # chatbot_response = response['choices'][0]['message']['content']
+    # conversation[-1]['content'] = chatbot_response  # Update conversation with the chatbot response
+    # return chatbot_response
 # Function to read the text content from a file
 def read_text_from_file(filename):
     with open(filename, 'r', encoding='utf-8') as file:
@@ -35,8 +34,6 @@ def chatbot_view(request):
         if user_input.lower() in ['exit', 'quit', 'bye']:
             return JsonResponse({'response': "Chatbot: Goodbye!"})
         
-        curr = time.time()
-        time_now = time.ctime(curr)
 
         # Step 1: Read the text content from the file
         filename = "media/rantzon.txt"  # Replace with the actual filename
@@ -44,7 +41,7 @@ def chatbot_view(request):
 
         # Initial conversation message with the system's introduction and the context from the file
         conversation = [
-            {"role": "system", "content": f"You are a helpful assistant for Health related suggestion who predicts diseases. You can ask more information if needed. Current time is {time_now}"},
+            {"role": "system", "content": f"You are a helpful assistant for Health related suggestion who predicts diseases. You can ask more information if needed."},
             {"role": "user", "content": preprocessed_text},
         ]
 
